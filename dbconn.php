b80fea4178b3c0bde111a0521523892959843bf1
leon-acor-gmail <leon.acor@gmail.com>
2017-05-06 02:36:20 -0500

    se hace la conexión por oop
    se realiza una clase que contiene los metodos de consulta a mysql

--- dbconn.php
+++ dbconn.php
@@ -1,6 +1,12 @@
 <?php 
 
-        $bConnection = new mysqli('localhost', 'domestic_usrvat', '5y5t3m45', 'domestic_vat');
+class InfoSystem
+{
+    //private bConnection = null;
+
+   public function getConn()
+   {
+       $bConnection = new mysqli('localhost', 'domestic_usrvat', '5y5t3m45', 'domestic_vat');
 		if ($bConnection->connect_errno) 
 		{
 			echo 'NOK: (Error: '.$mysqli->connect_errno.') '.$mysqli->connect_error;
@@ -10,5 +16,20 @@
 		{
 			echo 'OK '.$mysqli->host_info.'\n';
 			
-		}s
+		}
+        return $bConnection;
+   }
+
+   public function setMessage($strText)
+   {
+       echo ('<br> '.$strText);
+   }
+}
+
+$objIS = new InfoSystem();
+$objIS->getConn();
+$objIS->setMessage('mensaje desde oop');
+
+
+        
 ?>
